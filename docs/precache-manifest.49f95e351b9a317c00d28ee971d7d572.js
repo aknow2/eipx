d1aa5f7899fbfcbeeba2ec7c74f8913242918518
金山智昭 <kanayamatomoaki@kanayamatomoakinoiMac-2.local>
2019-11-21 19:40:33 +0900

    remove readme

---
+++ docs/precache-manifest.49f95e351b9a317c00d28ee971d7d572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68255502f1706eb19d47c889f0f9dd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ff2c5709a7987a43dc1",
+    "url": "./static/css/main.131bf279.chunk.css"
+  },
+  {
+    "revision": "ac4c8847aaac0156c96d",
+    "url": "./static/js/2.a04d4d08.chunk.js"
+  },
+  {
+    "revision": "4ff2c5709a7987a43dc1",
+    "url": "./static/js/main.b33a8153.chunk.js"
+  },
+  {
+    "revision": "eea3228cbfacfd4d5831",
+    "url": "./static/js/runtime-main.d9746f78.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90053e2cf640522e39fd8bd9bb39c76c.js"
+  "./precache-manifest.49f95e351b9a317c00d28ee971d7d572.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
